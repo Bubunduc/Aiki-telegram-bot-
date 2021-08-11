8732ce5b087ef7f5ca5cf5de0870799187d45b62
Bubunduc <86464565+Bubunduc@users.noreply.github.com>
2021-08-11 17:10:35 +0300

    Update Aiki_bot.py

--- Aiki_bot.py
+++ Aiki_bot.py
@@ -4,33 +4,37 @@ from telebot.types import ReplyKeyboardMarkup
 from youtubesearchpython import Search
 import requests
 import time
-bot = telebot.TeleBot('Your token')
+bot = telebot.TeleBot('1933230063:AAGUBfqzOK5rnMAm8PAPho6QtwaFgS7NfAQ')
 @bot.message_handler(commands=['start'])
 def Startbot(message):# Name for function not important
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button1 = types.KeyboardButton('Поиск техники')
     button2 = types.KeyboardButton('Айкимудрость')
-    markup.add(button1,button2)
+    button3 = types.KeyboardButton('Программа аттестации')
+    markup.add(button1,button2,button3)
     bot.send_message(message.chat.id, "Привет {}  {}! С помощь меня ты сможешь быстро и лекго найти нужную тебе технику айкидо,чтобы узнать подробнее напишите /help".format(message.chat.first_name,message.chat.last_name),reply_markup=markup)
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id,'Кнопка "Айкимудрость" генерирует случайную мудрость по теме айкидо c помощью сервиса балабоба')
     bot.send_message(message.chat.id,'С помощью кнопки "Поиск техники" можно получить необходимую вам технику(Желательно вводить название на английском)')
+    bot.send_message(message.chat.id,'При нажатии кнопки "Программа аттестации" будет отправлена ссылка на актуальную программу аттестации с сайта Honbu dojo')
 @bot.message_handler(content_types=['text'])
 # the function will be executed when sending any message to the bot, since handler with text type specified if message.text is important
 def some(message):
     if message.chat.type == 'private':
-        if message.text == 'Поиск техники':
+        if message.text.lower() == 'поиск техники':
             msg = bot.send_message(message.chat.id, 'Что будем искать?')
             bot.register_next_step_handler(msg, some_1)
-        if message.text == 'Айкимудрость':
+        if message.text.lower() == 'айкимудрость':
             bot.send_message(message.chat.id,'Составляю...')
-            time.sleep(3)
+            time.sleep(2)
             API_URL = 'https://yandex.ru/lab/api/yalm/text3'
             data = {"query": "Айкидо", "intro": 11, "filter": 1}
             request = requests.post(url=API_URL, json=data)
             request = request.json()
             bot.send_message(message.chat.id, request['query'] + request['text'])
+        if message.text.lower() =='программа аттестации':
+            bot.send_message(message.chat.id,'https://www.yoshinkan.net/05images/_pdf/sinsakamoku2021E.pdf')
 
 
 def some_1(message):
